d3fe703976a8386f589f74b1f018f05c1977ebd2
sowon901 <87898185+sowon901@users.noreply.github.com>
2023-12-19 17:53:55 +0900

    Create Admin.java
    Admin.java

---
+++ src/book_pocket/Admin.java
@@ -0,0 +1,22 @@
+package book_pocket;
+
+public class Admin extends Person{
+
+    private String id = "Admin";
+    private String password = "Admin1234";
+
+
+    public Admin(String name, String phone) {
+        super(name, phone);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+
+
+    public String getPassword() {
+        return password;
+    }
+}
