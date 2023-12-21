371ee0ae6234d11637ae9110fa64849aa5d5b2ef
jjuuuunn <didtdjwns111@gmail.com>
2023-12-21 17:12:48 +0900

    초기 화면 숫자로 입력받도록 변경

--- src/book_pocket/ShoppingMallServiceRunner.java
+++ src/book_pocket/ShoppingMallServiceRunner.java
@@ -13,20 +13,18 @@ import java.util.Optional;
 import java.util.Scanner;
 
 public class ShoppingMallServiceRunner {
-    static Scanner sc;
+    static Scanner sc = new Scanner(System.in);
     static ShoppingMallService shoppingMallService = new ShoppingMallService(sc);
     static AdminService adminService = new AdminService();
     static UserService userService = UserService.getInstance();
 
     public static void main(String[] args) throws Exception {
-        sc = new Scanner(System.in);
-
         boolean initLoop = true;
         while (initLoop) {
-            System.out.println("로그인 | 회원가입 | 종료");
+            System.out.println("1. 회원가입 | 2. 로그인 | 3. 종료");
             String initChoice = sc.nextLine();
 
-            if (initChoice.equals("회원가입")) { // 고객 회원가입
+            if (initChoice.equals("1")) { // 고객 회원가입
                 String id = "";
 
                 boolean duplicateName = true; // 이름이 중복이면 다시 입력 받게 반복
@@ -57,49 +55,44 @@ public class ShoppingMallServiceRunner {
                         .build();
                 userService.signUp(user); // UserList에 회원 정보 저장
 
-            } else if (initChoice.equals("로그인")) { // 로그인 시작
-                System.out.println("=====로그인 화면=====");
-                System.out.print("아이디를 입력해주세요 : ");
-                String id = sc.nextLine();
-                System.out.print("비밀번호를 입력해주세요 : ");
-                String password = sc.nextLine();
+            } else if (initChoice.equals("2")) { // 로그인 시작
+                SignInDto dto = getSignInDto();
+                Optional<User> optionalUser = userService.findByIdAndPassword(dto);
+                if (optionalUser.isEmpty()) {
+                    System.out.println("잘못된 계정입니다.");
+//                        throw new Exception("잘못된 계정입니다.");
+                }
+                User user = optionalUser.get();
 
-                SignInDto dto = new SignInDto(id, password);
+                welComeToShoppingMall();
 
-                if (id.length() > 6 && id.substring(0, 6).equals("Admin_")) { // 아이디가 관리자 아이디일 경우
+                ExistUserDto userDto = new ExistUserDto(user.getId());
+
+                boolean userLoop = true;
+                while (userLoop) {
+                    userLoop = shoppingMallService.displayMenu(userDto);
+
+                    Thread.sleep(1000);
+                }
+
+            } else if (initChoice.equals("3")) { // 프로그램 종료
+                initLoop = false;
+                System.out.println("프로그램을 종료합니다.");
+
+            } else if (initChoice.equals("4")) { // 아이디가 관리자 아이디일 경우
+                SignInDto dto = getSignInDto("관리자");
+
+                if (dto.getId().length() > 6 && dto.getId().substring(0, 6).equals("Admin_")) {
                     Optional<Admin> optionalAdmin = adminService.signIn(dto);
                     if (optionalAdmin.isPresent()) {
-
                         boolean adminLoop = true;
                         while (adminLoop) { // 관리자 메뉴 실행
                             adminLoop = shoppingMallService.adminMenu();
                         }
-                    } else {
-                        System.out.println("잘못된 계정입니다.");
-//                        throw new Exception("잘못된 계정입니다.");
                     }
-                } else { // 고객
-                    Optional<User> optionalUser = userService.findByIdAndPassword(dto);
-                    if (optionalUser.isEmpty()) {
-                        System.out.println("잘못된 계정입니다.");
-//                        throw new Exception("잘못된 계정입니다.");
-                    }
-                    User user = optionalUser.get();
-
-                    welComeToShoppingMall();
-
-                    ExistUserDto userDto = new ExistUserDto(user.getId());
-
-                    boolean userLoop = true;
-                    while (userLoop) {
-                        userLoop = shoppingMallService.displayMenu(userDto);
-
-                        Thread.sleep(1000);
-                    }
+                } else {
+                        throw new Exception("잘못된 계정입니다.");
                 }
-            } else if (initChoice.equals("종료")) { // 프로그램
-                initLoop = false;
-                System.out.println("프로그램을 종료합니다.");
             } else { // 잘못된 값을 입력된 경우
                 System.out.println("회원가입, 로그인, 종료 중에서 골라주세요.");
 //                throw new Exception("회원가입, 로그인, 종료 외의 값이 입력되었습니다.");
@@ -108,6 +101,20 @@ public class ShoppingMallServiceRunner {
             }
         }
         sc.close();
+    }
+
+    private static SignInDto getSignInDto() {
+        return getSignInDto("");
+    }
+
+    private static SignInDto getSignInDto(String s) {
+        System.out.printf("=====%s로그인 화면=====\n", s);
+        System.out.printf("%s아이디를 입력해주세요 : ", s);
+        String id = sc.nextLine();
+        System.out.printf("%s비밀번호를 입력해주세요 : ", s);
+        String password = sc.nextLine();
+
+        return new SignInDto(id, password);
     }
 
     private static void welComeToShoppingMall() {
